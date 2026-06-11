3515a4fa4f0837f0e3a5da6ca5d98d8f7bd80ce0
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-12 00:33:12 +0300

    Add project files.

---
+++ OpneAiGenDev/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var apiKey = config["OpenAI:ApiKey"]!;
+var client = new ChatClient("gpt-4o-mini", apiKey);
+
+var messages = new List<ChatMessage>();
+
+while (true)
+{
+    string prompt = Console.ReadLine();
+
+    messages.Add(new UserChatMessage(prompt));
+
+    var resp = client.CompleteChatStreamingAsync(messages);
+
+    var fullResponse = new StringBuilder();
+
+    await foreach (var item in resp)
+    {
+        foreach (var part in item.ContentUpdate)
+        {
+            Console.Write(part.Text);
+            fullResponse.Append(part.Text);
+        }
+    }
+    Console.WriteLine();
+
+    messages.Add(new AssistantChatMessage(fullResponse.ToString()));
+}
